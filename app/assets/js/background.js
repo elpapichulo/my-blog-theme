d4971f2b544113d1c427cb87b92bd17f2c4b9d21
sota0805 <sota.yamashita@gmail.com>
2014-01-06 18:00:03 +0900

    Create background.js to change background

--- app/assets/js/background.js
+++ app/assets/js/background.js
@@ -7,3 +7,23 @@ Todo
 - Set timer
  - Change background image by time
  */
+
+(function(){
+	var backgroundArray = [
+		["bike", "https://dl.dropboxusercontent.com/u/74344418/Sota'sBlog/cover1.jpg"],
+		["starlit sky","https://dl.dropboxusercontent.com/u/74344418/Sota'sBlog/cover2.jpg"],
+		["sea","https://dl.dropboxusercontent.com/u/74344418/Sota'sBlog/cover3.jpg"],
+		["gateway", "https://dl.dropboxusercontent.com/u/74344418/Sota'sBlog/cover4.jpg"]
+	];
+
+	var background = $("header");
+
+	var data = new Date();
+	// Confirm
+	console.log(data);
+
+	
+
+
+
+});
